4cc9febb3ed458fbb7eb3f3f43d6278816ab83b3
spbovarnick <spbovarnick@gmail.com>
2025-02-25 13:32:53 -0800

    new dataset

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,7 +2,7 @@ const getDataset = () => {
   let dataset, errorMessage;
   if (process.env.NODE_ENV === "development") {
     // return assertValue(process.env.NEXT_PUBLIC_SANITY_DEV_DATASET as string, "Missing environment variable: NEXT_PUBLIC_SANITY_DEV_DATASET")
-    dataset = process.env.NEXT_PUBLIC_SANITY_DEV_DATASET;
+    dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
     errorMessage = "Missing environment variable: NEXT_PUBLIC_SANITY_DEV_DATASET"
   }
   if (process.env.NODE_ENV === "production") {
