a2525c2c61894ec6738dc9035fc7a71eef40d22a
spbovarnick <spbovarnick@gmail.com>
2025-02-24 16:59:31 -0800

    updated dataset getter in env.ts

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,10 +1,21 @@
+const getDataset = () => {
+  let dataset, errorMessage;
+  if (process.env.NODE_ENV === "development") {
+    // return assertValue(process.env.NEXT_PUBLIC_SANITY_DEV_DATASET as string, "Missing environment variable: NEXT_PUBLIC_SANITY_DEV_DATASET")
+    dataset = process.env.NEXT_PUBLIC_SANITY_DEV_DATASET;
+    errorMessage = "Missing environment variable: NEXT_PUBLIC_SANITY_DEV_DATASET"
+  }
+  if (process.env.NODE_ENV === "production") {
+    dataset = process.env.NEXT_PUBLIC_SANITY_PROD_DATASET;
+    errorMessage = "Missing environment variable: NEXT_PUBLIC_SANITY_PROD_DATASET"
+  }
+  return assertValue(dataset as string, errorMessage as string)
+};
+
+export const dataset = getDataset();
+
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-02-12'
-
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
