bb54b86275913247b7d939b22a6d9cb88a295e84
spbovarnick <spbovarnick@gmail.com>
2026-02-27 20:55:22 -0800

    fix to live

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,5 +1,4 @@
 import { createClient } from 'next-sanity'
-import { defineLive } from 'next-sanity/live'
 
 import { apiVersion, dataset, projectId } from '../env'
 
@@ -10,13 +9,3 @@ export const client = createClient({
   useCdn: process.env.NODE_ENV === "development" ? true : false,
 
 })
-const token = process.env.SANITY_API_READ_TOKEN;
-if (!token) {
-  throw new Error("missing SANITY_API_READ_TOKEN")
-}
-
-export const {SanityLive} = defineLive({
-  client,
-  serverToken: token,
-  browserToken: token,
-});
