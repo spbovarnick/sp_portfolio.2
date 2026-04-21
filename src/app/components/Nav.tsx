e5238f774a0d962a984e9259288cedd7eeed3253
spbovarnick <spbovarnick@gmail.com>
2026-04-21 12:53:24 -0700

    updates to INFO page

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,16 +2,35 @@
 import Link from "next/link";
 import NameBanner from "../../../public/nameBanner";
 
-const Nav = () => {
+interface NavProps {
+  page: string
+}
+
+const Nav = ({page}: NavProps) => {
+
+  const whiteBannerPages = [
+    "info",
+    "home",
+    "project"
+  ];
+
+  const whiteLinksPages = [
+    "home",
+    "project",
+  ];
+
+  const bannerColor = whiteBannerPages.includes(page) ? "white" : "black";
+
+  const linksColor = whiteLinksPages.includes(page) ? "text-white" : "text-black";
 
   return (
     <nav className={`fixed z-10 w-full flex flex-col md:grid grid-cols-2 gap-0 pt-[5.4vh] px-[12vw] md:px-10 md:pt-[52px]`}>
       <div className="w-full md:w-2/3">
         <NameBanner
-          color={"white"}
+          color={bannerColor}
         />
       </div>
-      <div className="text-center text-white mt-5 md:m-0 md:relative md:top-8 h-fit md:flex md:justify-end md:pr-16">
+      <div className={`${page === "info" ? "text-white md:text-black" : linksColor} text-center mt-5 md:m-0 md:relative md:top-8 h-fit md:flex md:justify-end md:pr-16`}>
         <Link className="" href={"/info"}>INFO</Link>
         <Link className="ml-12" href={"/portfolio"}>PROJECTS</Link>
       </div>
--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,7 +1,6 @@
 import { sanityFetch } from "../lib/sanityFetch"
 import { InfoPageQueryResult, TaglineQueryResult } from "@/sanity/types"
 import { infoPageQuery, taglineQuery } from "../lib/queries"
-import InfoPageNav from "../components/InfoPageNav";
 import { PortableText } from "@portabletext/react";
 import { urlFor } from "@/sanity/lib/image";
 import Image from "next/image";
@@ -20,44 +19,45 @@ export default async function InfoPage({}){
 
   return (
     <>
-    <div className="text-center md:pt-0 flex flex-col items-center uppercase">
-      <InfoPageNav />
-      <div className="info flex flex-col items-center content-center w-[80vw] md:w-[40vw] mt-10 md:mt-20">
-        {infoContent?.portrait &&
-            <Image
-              className="object-cover mb-8"
-              src={urlFor(infoContent?.portrait)
-                .width(1000)
-                .height(666)
-                .dpr(3)
-                .quality(80)
-                .url()
-              }
-              width={1000}
-              height={666}
-              placeholder="blur"
-              sizes="(max-width: 768px) 100vw, 50vw"
-              quality={80}
-              alt={'Portrait of Sarita Posada'}
-              blurDataURL={infoContent?.portrait.asset?.metadata?.lqip}
-            />
+      <div className="flex flex-col uppercase min-h-screen md:grid md:grid-cols-2">
+        <div className="w-full md:h-screen h-[60vh]">
+          {infoContent?.portrait &&
+              <Image
+                className="object-cover w-full h-full"
+                src={urlFor(infoContent?.portrait)
+                  .width(1000)
+                  .height(666)
+                  .dpr(3)
+                  .quality(80)
+                  .url()
+                }
+                width={1000}
+                height={666}
+                placeholder="blur"
+                sizes="(max-width: 768px) 100vw, 50vw"
+                quality={80}
+                alt={'Portrait of Sarita Posada'}
+                blurDataURL={infoContent?.portrait.asset?.metadata?.lqip}
+              />
+            }
+        </div>
+        <div className="w-full flex-1 md:h-screen flex flex-col justify-center items-center">
+          {tagline?.copy &&
+            <div id="tagline" className="px-[34px] md:px-0 md:w-[40vw]">
+              <PortableText value={tagline.copy} />
+            </div>
           }
-        {tagline?.copy &&
-          <div id="tagline" className="px-[34px] md:px-0 md:w-[40vw]">
-            <PortableText value={tagline.copy} />
+          <div className="mt-4">
+              INQUIRIES: <a href="mailto:info@saritaposada.com" target="_blank" className="underline">office@saritaposada.com</a>
           </div>
-        }
-        <div className="mt-4">
-            INQUIRIES: <a href="mailto:info@saritaposada.com" target="_blank" className="underline">office@saritaposada.com</a>
-        </div>
-        <div className="">
-            PRESS: <a href="mailto:OLIVIA@OLIVIALUGARINI.COM" target="_blank" className="underline">OLIVIA@OLIVIALUGARINI.COM</a>
-        </div>
-        <div className="mt-4">
-            &#169; saritaposada
+          <div className="">
+              PRESS: <a href="mailto:OLIVIA@OLIVIALUGARINI.COM" target="_blank" className="underline">OLIVIA@OLIVIALUGARINI.COM</a>
+          </div>
+          <div className="mt-4">
+              &#169; saritaposada
+          </div>
         </div>
       </div>
-    </div>
     </>
   )
 }
